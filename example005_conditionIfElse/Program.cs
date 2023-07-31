d0e346dc783fc7c1b1cd4ca439d65510703fa99e
Dmitry <sicily007@rambler.ru>
2023-07-31 22:01:52 +0300

    example004_addIfElse

---
+++ example005_conditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+if (username.ToLower() == "миша")
+{
+    Console.WriteLine("Привет, Мишутка!!!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}

d552f4ded92fc979dc5f0de77cc8766ae56afe8e
Dmitry <sicily007@rambler.ru>
2023-07-31 21:45:11 +0300

    example003_add randomize

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 100); //числа от 1 до 99
+int numberB = new Random().Next(1, 100);
 int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(result);
